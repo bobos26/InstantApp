35a08f70d5f49584b250ee04bcfc43ccdafbfc26
이주성(Lee Jusung)/Project 4실/SKP <jusunglee@sk.com>
2017-07-04 11:23:02 +0900

    first commit

---
+++ feature/src/main/java/com/skplanet/vertical/instantapp/feature/MainActivity.java
@@ -0,0 +1,19 @@
+package com.skplanet.vertical.instantapp.feature;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to handle app links.
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
+    }
+}
